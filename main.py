90c3d7cfa2be44c968c9fa458d64991c0085969e
lyubomirvelchev <lyubomirvelchev98@gmail.com>
2023-02-13 13:57:54 +0200

    docker added

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import time
-from hello_branch import hello
-from real_branch import f
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    counter = 0
-    while counter < 20:
-        hello()
-        f()
-        counter += 1
-        time.sleep(2)
+    app.run(host='0.0.0.0', port=5000)
